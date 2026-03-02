efd4e62ca56a5942f221e2df6272aad3e08ca677
domenicodev <domenico.romeo@mystenlabs.com>
2026-03-02 09:37:44 +0100

    chore: Update comment on getHeroSwordIds for E1

--- E1/mint-hero/src/helpers/getHeroSwordIds.ts
+++ E1/mint-hero/src/helpers/getHeroSwordIds.ts
@@ -4,7 +4,7 @@ import { suiClient } from "../suiClient";
 /**
  * Gets the dynamic object fields attached to a hero object by the object's id.
  * To get the names dynamic fields and dynamic object fields, we use the listDynamicFields method.
- * To get the Object IDs of the dynamic object fields, we use the getDynamicObjectField method.
+ * To get the Object IDs of the dynamic object fields, we use the field "childId".
  * For the scope of this exercise, we ignore pagination, and just fetch the first page.
  * Filters the objects and returns the object ids of the swords.
  */
